1eb1046e8264a41ded9d49f006c36fbdbd5c27c6
Taranveer Singh <taranveersingh93@gmail.com>
2023-07-20 23:21:32 -0400

    Make logo component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import Logo from './Logo/Logo';
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <Logo />
+    </div>
+  )
+}
+
+export default Navbar;
