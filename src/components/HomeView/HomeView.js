5177ea8fc6efa9c1ab3f99c97918d1dc98fa66a8
Taranveer Singh <taranveersingh93@gmail.com>
2023-07-21 02:30:09 -0400

    Define parent elements for the categories grid

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -3,7 +3,17 @@ import IntroText from './IntroText/IntroText';
 
 const HomeView = () => {
   return (
-    <IntroText />
+    <div className='home-view'>
+      <IntroText />
+      <div className='categories'>
+        <section className='category'>Category A</section>
+        <section className='category'>Category A</section>
+        <section className='category'>Category A</section>
+        <section className='category'>Category A</section>
+        <section className='category'>Category A</section>
+        <section className='category'>Category A</section>
+      </div>
+    </div>
   )
 }
 
