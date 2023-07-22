21f91b1c530cc9411ea6b8e5cd3be20cbdd9cc63
Seth Burns <seth@sethburns.com>
2023-07-21 17:23:04 -0700

    Add progress to new task page

--- src/components/SelectTaskView/SelectTaskView.js
+++ src/components/SelectTaskView/SelectTaskView.js
@@ -1,7 +1,67 @@
-
+import tasks from '../../data/data';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const SelectTaskView = () => {
-  return <></>
-}
+  const { category } = useParams();
+
+  const [currentTasks, setCurrentTasks] = useState([]);
+  const [unseenTasks, setUnseenTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState('');
+
+  const getTasks = (cat) => {
+    if (cat !== 'all') {
+      setCurrentTasks(tasks[cat]);
+    } else {
+      let allTasks = [];
+      tasks.forEach((task) => {
+        allTasks.push(...tasks[task]);
+      });
+      setCurrentTasks(allTasks);
+    }
+    
+  };
+
+  useEffect(() => {
+    getTasks(category);
+  }, []);
+
+  const getUnseenTasks = (tasks) => {
+    const filteredTasks = tasks.filter((task) => {
+      return !task.seen;
+    });
+    setUnseenTasks(filteredTasks);
+  };
+
+  useEffect(() => {
+    getUnseenTasks(currentTasks);
+  }, [currentTasks]);
+
+  const getCurrentTask = (options) => {
+    let randomIndex = Math.floor(Math.random() * options.length);
+    // options[randomIndex].seen = true;
+    setCurrentTask(options[randomIndex]);
+  };
+
+  useEffect(() => {
+    getCurrentTask(unseenTasks);
+  }, []);
+
+  console.log(currentTasks)
+  console.log(unseenTasks)
+  
+  return (
+    <div className="new-task-page">
+      <div className="task-card">
+        {/* <p className="task-text">${currentTask.text}</p> */}
+        <div className="task-card-buttons">
+          <button className="deny-button"></button>
+          <button className="accept-button"></button>
+        </div>
+      </div>
+      <button className="back-button"></button>
+    </div>
+  );
+};
 
 export default SelectTaskView;
