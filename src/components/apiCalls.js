319eb68dbcf16a8fbefc220cce6070465d8cbbb0
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-25 09:40:54 -0600

    feat: add general fetch call on seperate file

---
+++ src/components/apiCalls.js
@@ -0,0 +1,15 @@
+
+async function fetchTasks() {
+  const response = await fetch('http://localhost:3001/api/v1/tasks')
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+const data = await response.json()
+return data
+}
+
+async function fetchCatergoryTask(category) {
+  return (
+
+  )
+}
