0d6455fdb0ea6176d768716ee61f2efa395d21fb
Seth Burns <seth@sethburns.com>
2023-07-25 15:46:24 -0700

    Update tests for new tasks

--- cypress/e2e/my-tasks-spec.cy.js
+++ cypress/e2e/my-tasks-spec.cy.js
@@ -1,8 +1,22 @@
 describe('my tasks page spec', () => {
+  const categories = ['Exercise', 'Cleaning', 'Organization', 'Work', 'Mental Care', 'Health'];
   beforeEach(() => {
     cy.visit('localhost:3000/tasklist');
   });
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  it('displays a table with appropriate headings', () => {
+    cy.get('thead tr')
+      .should('contain', 'Category')
+      .and('contain', 'Task')
+      .and('contain', 'Actions');
+  });
+  it('should include one of six categories in the table for each task', () => {
+    cy.get('tbody tr').each(($row) => {
+      cy.wrap($row)
+        .find('td:first')
+        .invoke('text')
+        .then((text) => {
+          expect(categories).to.include(text.trim());
+        });
+    });
+  });
+});
--- cypress/e2e/new-task-spec.cy.js
+++ cypress/e2e/new-task-spec.cy.js
@@ -1,5 +1,13 @@
-// describe('new task spec', () => {
-//   it('should take you to one of the six categories you', () => {
-//     cy.visit('localhost:3000')
-//   })
-// })
+describe('new tasks (categories) page spec', () => {
+  const categories = ['Exercise', 'Cleaning', 'Organization', 'Work', 'Mental Care', 'Health'];
+
+  categories.forEach((category) => {
+    const categoryURL = (category.charAt(0).toLowerCase() + category.slice(1)).replace(" ", "")
+    
+    it(`should have an h1 of ${category} when at the url for that category`, () => {
+      cy.visit(`localhost:3000/${categoryURL}`)
+        .get('.new-task-page')
+        .contains('h1', category);
+    });
+  });
+});
