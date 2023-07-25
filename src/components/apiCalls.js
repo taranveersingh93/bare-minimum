d7622f363a0122c68de8ce369a33848b4ff31fae
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-25 09:42:06 -0600

    feat: add specific category fetch call

--- src/components/apiCalls.js
+++ src/components/apiCalls.js
@@ -4,12 +4,15 @@ async function fetchTasks() {
   if (!response.ok) {
     throw new Error(response.statusText)
   }
-const data = await response.json()
-return data
+  const data = await response.json()
+  return data
 }
 
 async function fetchCatergoryTask(category) {
-  return (
-
-  )
+  const response = await fetch(`http://localhost:3001/api/v1/tasks/${category}`)
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  const data = await response.json()
+  return data
 }
