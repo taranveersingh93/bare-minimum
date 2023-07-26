78109fe8b223e751c7e5f0b356d40fe64770dc1f
Seth Burns <seth@sethburns.com>
2023-07-25 18:07:13 -0700

    Add fixtures for stubbing

--- cypress/e2e/new-task-spec.cy.js
+++ cypress/e2e/new-task-spec.cy.js
@@ -4,6 +4,10 @@ describe('new tasks (categories) page spec', () => {
   categories.forEach((category) => {
     const categoryURL = (category.charAt(0).toLowerCase() + category.slice(1)).replace(' ', '');
     it(`should have an h1 of ${category} when at /${categoryURL}`, () => {
+      cy.intercept('GET', `http://localhost:3001/api/v1/tasks/${categoryURL}`, {
+      statusCode: 200,
+      fixture: `${categoryURL}TestData`,
+    })
       cy.visit(`localhost:3000/${categoryURL}`).get('.new-task-page').contains('h1', category);
     });
   });
