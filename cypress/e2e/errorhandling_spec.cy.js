be1497cbb882e037d15a79bc8808b05848ef7c25
Calli Herrmann <calliherrmann@gmail.com>
2023-07-28 18:40:36 -0400

    Add 500 error handling

--- cypress/e2e/errorhandling_spec.cy.js
+++ cypress/e2e/errorhandling_spec.cy.js
@@ -1,34 +1,32 @@
-describe('Error handling', () => {
+describe('Error handling for wrong pages', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://bare-minimum-api-53c62eb03bf8.herokuapp.com/api/v1/savedtasks', {
-      statusCode: 200,
-      body: [],
-    }).as('getSavedTasks');
-    cy.intercept('GET', `https://bare-minimum-api-53c62eb03bf8.herokuapp.com/api/v1/tasks/bear`, {
+    cy.intercept('GET', `https://bare-minimum-api-53c62eb03bf8.herokuapp.com/api/v1/savedtasks`, {
       statusCode: 404,
     }).as('pageNotFound');
-    cy.intercept('GET', `https://bare-minimum-api-53c62eb03bf8.herokuapp.com/api/v1/tasks`, {
-      statusCode: 200,
-    }).as('getTask');
   });
-  it('Should show an error on page when user navigates to incorrect category', () => {
-    cy.visit('localhost:3000/bear')
-    .wait(['@getSavedTasks', '@pageNotFound', '@getTask'])
-    .get('h1').should('have.text', 'Loading...')
-    .get('.error-message').contains('We apologize! Error: Not Found. Please try again later.')
-  })
-  it('Should show a 404 error on page when user navigates to any other wrong page', () => {
+  it('Should show an error on page when user navigates to any other wrong page', () => {
     cy.visit('localhost:3000/bear/bear')
-    .wait('@getSavedTasks')
+    .wait('@pageNotFound')
     .get('.error-bear').should('exist')
   })
 })
 
-describe('sad path testing', () => {
+describe('500 Error', () => {
+  it('Should show an error on page when server is down', () => {
+  cy.intercept('GET', 'https://bare-minimum-api-53c62eb03bf8.herokuapp.com/api/v1/savedtasks', {
+    statusCode: 500,
+  }).as('serverDown');
+  cy.visit('localhost:3000')
+  .wait('@serverDown')
+  .get('.error-bear').should('exist')
+})
+})
+
+describe('Error handling for categories', () => {
   const categories = ['Exercise', 'Cleaning', 'Organization', 'Work', 'Mental Care', 'Health'];
 
   categories.forEach((category) => {
-    it(`should show Loading error on ${category} page if fetch fails`, () => {
+    it(`Should show Loading error on ${category} page if fetch fails`, () => {
       const categoryURL = (category.charAt(0).toLowerCase() + category.slice(1)).replace(' ', '');
       cy.intercept(`https://bare-minimum-api-53c62eb03bf8.herokuapp.com/api/v1/tasks/${categoryURL}`, {
         statusCode: 404,
